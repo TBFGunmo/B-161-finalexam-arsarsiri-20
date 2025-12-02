c65244da58f48d39bbe68619f7e9e2f9e07ffab8
TBFGunmo <gunmohome123@gmail.com>
2025-12-02 15:36:58 +0700

    Item base class

--- Assets/Script/SpeedBoost.cs
+++ Assets/Script/SpeedBoost.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
-public class SpeedBoost : MonoBehaviour
+public class SpeedBoost : PowerUpBase
 {
+    public override void ApplyEffect(Player target)
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
