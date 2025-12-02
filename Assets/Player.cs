1c7521cd65baf6f0ed54a1b1e4f2ab9e0b006026
TBFGunmo <gunmohome123@gmail.com>
2025-12-02 15:23:16 +0700

    add tacher script walk and jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // public Fields
+    private float moveSpeed = 5f; // speed movement
+    private float jumpForce = 10f; // basic jump
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 1. movement (left-right using A/D/arrow keys)
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        // move left-right base on MoveSpeed
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        // 2. Jump (No gound Check Requied)
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
     }
 }
